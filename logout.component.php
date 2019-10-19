7510472a2154e7ff6342dfd4ed920732f65a3ef9
VikasPHP <vikasphpdeveloper@gmail.com>
2019-10-19 21:48:59 +0530

    delete all files

--- logout.component.php
+++
@@ -1,8 +0,0 @@
-<?php
-// logout.component
-// session_start(); 10,838.99 pearlinn321
-if (isset($_SESSION['customerName']) || isset($_SESSION['userRole'])) {
-    session_destroy();
-    header("Location: http://localhost/Site/jnj_bi_project/");
-}
-
